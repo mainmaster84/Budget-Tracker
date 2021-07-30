ca5e9d166eb06c28b80b4d2d7ff2ea749de93846
Ronald Main <rbmain84@yahoo.com>
2021-07-30 13:33:28 -0500

    updates

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,11 +2,10 @@ const APP_PREFIX = 'BudgetTracker-';
 const VERSION = 'version_01';
 const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
-  "./models/transaction.js",
-  "./public/index.html",
-  "./public/css/styles.css",
-  "./public/js/idb.js",
-  "./public/js/index.js"
+  "./index.html",
+  "./css/styles.css",
+  "./js/idb.js",
+  "./js/index.js"
 ];
 
 // Respond with cached resources
